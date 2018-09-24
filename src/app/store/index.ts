6a7718bff7ab4e489dac6e2879eb6dfc7b70efed
Rajagopal <rakanc@outlook.com>
2018-09-24 21:00:45 +0530

    Initial Draft

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -4,6 +4,28 @@ import thunk from 'redux-thunk';
 import { LoadCountries } from '../actions/countries';
 import rootReducer from '../reducers';
 
+const logger = (store: any) => (next: any) => (action: any) => {
+  console.log('dispatching', action)
+  const result = next(action)
+  console.log('next state', store.getState())
+  return result
+}
+​
+const crashReporter = (store: any) => (next: any) => (action: any)  => {
+  try {
+    return next(action)
+  } catch (err) {
+    console.error('Caught an exception!', err)
+    // Raven.captureException(err, {
+    //   extra: {
+    //     action,
+    //     state: store.getState()
+    //   }
+    // })
+    throw err
+  }
+}
+
 const configStore = (initialState?: any) => {
 
   // if (!initialState) {
@@ -17,7 +39,7 @@ const configStore = (initialState?: any) => {
   return createStore(
     rootReducer,
     initialState!,
-    applyMiddleware(thunk, reduxImmutableStateInvariant()));
+    applyMiddleware(logger, crashReporter, thunk, reduxImmutableStateInvariant()));
 }
 
 const rootStore = configStore();
